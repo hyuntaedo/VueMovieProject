b377378eebe617bfd3a6f6b66e24cf915abfad35
Dohyuntae <dohyuntae123@gmail.com>
2021-09-17 09:55:42 +0900

    search기능추가

---
+++ src/components/MovieList.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="container">
+		<div class="inner">
+		<MovieItem 
+		v-for="movie in movies"
+		:key="movie.idmbID"
+		:movie="movie" />
+		</div>	
+	</div>	
+</template>
+
+<script>
+import MovieItem from './MovieItem.vue'
+export default {
+	components: {
+		MovieItem
+	},
+	computed: {
+		movies() {
+			return this.$store.state.movie.movies
+		}
+	}
+}
+</script>
---
+++ src/store/movie.js
@@ -0,0 +1,36 @@
+//영화검색
+import axios from 'axios'
+export default({
+	namespaced: true,
+	state: () => ({
+		movies : [],
+		message: '',
+		loading: false
+	}),
+	getters: {
+
+	},
+	mutations: {
+		updateState(state,payload) {
+			//['movies','message','loading']
+			Object.keys(payload).forEach(key =>{
+				state[key] = payload[key]
+			})
+		},
+		resetMovies(state){
+			state.movies = []
+		}
+	},
+	actions: {
+		async searchMovies({commit}, payload) {
+			const {title, type, year} = payload
+			const OMDB_API_KEY = '7035c60c'
+			
+			const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+			const {Search} = res.data
+			commit('updateState',{
+				movies: Search,
+			})
+		}
+	}
+})
