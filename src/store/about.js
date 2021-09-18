6527b502b144cb93d62804960008edffaf361fda
Dohyuntae <dohyuntae123@gmail.com>
2021-09-18 19:13:05 +0900

    about추가

--- src/store/about.js
+++ src/store/about.js
@@ -1,1 +1,11 @@
 //사용자정보
+export default {
+	namespaed: true,
+	state: () => ({
+		name: 'Kuma',
+		email: 'dohyuntae123@gmail.com',
+		blog: 'https://github.com/hyuntaedo',
+		phone: '010-4822-2652',
+		image: 'https://avatars.githubusercontent.com/u/44853842?v=4'
+	})
+}
